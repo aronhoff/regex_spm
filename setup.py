71af7a0ce08cf5bf6cabfe17d86e6a268c43db20
Aron Hoffmann <aron.m.hoff@gmail.com>
2022-06-17 10:47:05 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='regex_spm',
+  version='1.0.0',
+  packages=['regex_spm'],
+  url='https://github.com/aronhoff/regex_spm',
+  license='MIT',
+  author='aronhoffmann',
+  author_email='aron.m.hoff+pypy@gmail.com',
+  description='Enable Structural Pattern Matching for Python regular expressions'
+)
