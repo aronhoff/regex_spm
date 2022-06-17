d091706dee16ff51781d4d51189c6134e2e08b34
Aron Hoffmann <aron.m.hoff@gmail.com>
2022-06-17 10:52:00 +0200

    Extend setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
 setup(
   name='regex_spm',
   version='1.0.0',
@@ -8,5 +11,14 @@ setup(
   license='MIT',
   author='aronhoffmann',
   author_email='aron.m.hoff+pypy@gmail.com',
-  description='Enable Structural Pattern Matching for Python regular expressions'
+  description='Enable Structural Pattern Matching for Python regular expressions',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
+  python_requires='>=3.10',
 )
