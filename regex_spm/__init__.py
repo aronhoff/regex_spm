0bb6b8aee94b7ff8a4b53b3f381d0386381f3cbe
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-07-04 18:38:18 +0200

    🧹 Indented files according to PEP8 E111 (indentation should be a multiple of 4)

--- regex_spm/__init__.py
+++ regex_spm/__init__.py
@@ -4,15 +4,15 @@ from .regex_spm_match import RegexSpmMatch
 
 
 def search_in(string: str) -> RegexSpmMatch:
-  return RegexSpmMatch(string, _match_func=re.search)
+    return RegexSpmMatch(string, _match_func=re.search)
 
 
 def match_in(string: str) -> RegexSpmMatch:
-  return RegexSpmMatch(string, _match_func=re.match)
+    return RegexSpmMatch(string, _match_func=re.match)
 
 
 def fullmatch_in(string: str) -> RegexSpmMatch:
-  return RegexSpmMatch(string, _match_func=re.fullmatch)
+    return RegexSpmMatch(string, _match_func=re.fullmatch)
 
 
 __all__ = ['RegexSpmMatch', 'search_in', 'match_in', 'fullmatch_in']
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup
 
 with open('README.md', 'r') as f:
-  long_description = f.read()
+    long_description = f.read()
 
 setup(
-  name='regex_spm',
-  version='1.0.0',
-  packages=['regex_spm'],
-  url='https://github.com/aronhoff/regex_spm',
-  license='MIT',
-  author='aronhoffmann',
-  author_email='aron.m.hoff+pypy@gmail.com',
-  description='Enable Structural Pattern Matching for Python regular expressions',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-  ],
-  python_requires='>=3.10',
+    name='regex_spm',
+    version='1.0.0',
+    packages=['regex_spm'],
+    url='https://github.com/aronhoff/regex_spm',
+    license='MIT',
+    author='aronhoffmann',
+    author_email='aron.m.hoff+pypy@gmail.com',
+    description='Enable Structural Pattern Matching for Python regular expressions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.10',
 )
