2ac5b564a60e72ad0e0ac9861c0a0390e228ebf3
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-07-04 19:19:13 +0200

    ⏫ Bumped version to `1.0.1`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='regex_spm',
-    version='1.0.0',
+    version='1.0.1',
     packages=['regex_spm'],
     package_data={'regex_spm': ['py.typed']},
     url='https://github.com/aronhoff/regex_spm',
